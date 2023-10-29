545dec4760fc66fb655429caf5f25e64dd4dacfa
monu joshi <109272407+monu471@users.noreply.github.com>
2023-10-30 00:07:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-movies_dict = pickle.load(open("moviedict.pkl", "rb"))
+movies_dict = pickle.load(open("movies_d.pkl", "rb"))
 movies = pd.DataFrame(movies_dict)
 similarity = pickle.load(open("similarity.pkl", "rb"))
 
